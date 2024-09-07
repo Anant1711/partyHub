c4e2823a2682f0193958b8406b8ecee263ac0d9c
ACHAUHA2 <achauha2@visteon.com>
2024-09-07 20:06:16 +0530

    make changes in profileScreen.dart

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -19,9 +19,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
         child: FutureBuilder<String?>(
           future: getUserName(),
